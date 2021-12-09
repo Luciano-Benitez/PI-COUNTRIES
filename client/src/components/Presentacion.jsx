50808edea43a4b623819db40dac09da541ef4fa0
Luciano-Benitez <lbenitez19@hotmail.com>
2021-12-09 13:29:39 -0300

    PI-COUNTRIES

--- client/src/components/Presentacion.jsx
+++ client/src/components/Presentacion.jsx
@@ -4,8 +4,8 @@ import style from './Css/Presentacion.module.css';
 
 export function Presentacion(){
     return(
-        <div  >
-            <div   >
+        <div className={style.app} >
+            <div>
             <Link exact to={'/home'} ><h1>Henry Countries</h1></Link>
             </div>
         </div>
